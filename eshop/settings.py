0fa547964bf6b319a26fa21fd997501d88f75617
ankesh mahagaonkar <59524027+ankesh2108@users.noreply.github.com>
2021-02-16 22:33:15 +0530

    change

--- eshop/settings.py
+++ eshop/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2r_k*g-&9cu(7i=edj9nph3hiln48bck3_$deooq@i^ra&6$z='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','fashionstreet-ankesh.herokuapp.com']
 #'127.0.0.1','AnkeshEshop.com'
 
 # Application definition
